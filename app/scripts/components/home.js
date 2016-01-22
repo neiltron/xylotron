94aad4284491abb41e6d1fc22ed23ee9e9cef9bb
Neil Pullman <neil@descend.org>
2016-01-21 20:00:51 -0500

    add more brands

--- app/scripts/components/home.js
+++ app/scripts/components/home.js
@@ -9,39 +9,75 @@ export default class extends React.Component {
         {
           key: 'q',
           keycode: 81,
-          text: 'louis vuitton',
+          text: 'Louis Vuitton',
           pronunciation: 'louis   vwee ton',
           color: '#fefefe'
         }, {
           key: 'w',
           keycode: 87,
-          text: 'prada',
+          text: 'Prada',
           pronunciation: 'prada',
           color: '#fcfcfc'
         }, {
           key: 'e',
           keycode: 69,
-          text: 'gucci',
+          text: 'Gucci',
           pronunciation: 'gucci',
           color: '#fafafa'
         }, {
           key: 'a',
           keycode: 65,
-          text: 'chanel',
-          pronunciation: 'shanelle',
+          text: 'Chanel',
+          pronunciation: 'shanell',
           color: '#fdfdfd'
         }, {
           key: 's',
           keycode: 83,
-          text: 'fendi',
+          text: 'Fendi',
           pronunciation: 'fendi',
           color: '#fbfbfb'
         }, {
           key: 'd',
           keycode: 68,
-          text: 'armani',
+          text: 'Armani',
           pronunciation: 'armani',
           color: '#f9f9f9'
+        }, {
+          key: 'z',
+          keycode: 90,
+          text: 'Givenchy',
+          pronunciation: 'givenchy',
+          color: '#fcfcfc'
+        }, {
+          key: 'x',
+          keycode: 88,
+          text: 'Hermès',
+          pronunciation: 'Hermès',
+          color: '#fafafa'
+        }, {
+          key: 'c',
+          keycode: 67,
+          text: 'Christian Dior',
+          pronunciation: 'christian dior',
+          color: '#f8f8f8'
+        }, {
+          key: 'r',
+          keycode: 82,
+          text: 'Balenciaga',
+          pronunciation: 'balenciaga',
+          color: '#fbfbfb'
+        }, {
+          key: 't',
+          keycode: 84,
+          text: 'Cartier',
+          pronunciation: 'Cartier',
+          color: '#f9f9f9'
+        }, {
+          key: 'y',
+          keycode: 89,
+          text: 'Bulgari',
+          pronunciation: 'bulgari',
+          color: '#f7f7f7'
         }
       ]
     };
