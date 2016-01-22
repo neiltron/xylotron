a51de78325abe3f5ec0d0938540a30f3b6329b78
Neil Pullman <neil@descend.org>
2016-01-22 16:05:31 -0500

    move pad data to its own file

---
+++ app/scripts/pads.js
@@ -0,0 +1,75 @@
+export default [
+  {
+    key: 'q',
+    keycode: 81,
+    text: 'Louis Vuitton',
+    pronunciation: 'louis   vwee ton',
+    color: '#fefefe'
+  }, {
+    key: 'w',
+    keycode: 87,
+    text: 'Prada',
+    pronunciation: 'prada',
+    color: '#fcfcfc'
+  }, {
+    key: 'e',
+    keycode: 69,
+    text: 'Gucci',
+    pronunciation: 'gucci',
+    color: '#fafafa'
+  }, {
+    key: 'a',
+    keycode: 65,
+    text: 'Chanel',
+    pronunciation: 'shanell',
+    color: '#fdfdfd'
+  }, {
+    key: 's',
+    keycode: 83,
+    text: 'Fendi',
+    pronunciation: 'fendi',
+    color: '#fbfbfb'
+  }, {
+    key: 'd',
+    keycode: 68,
+    text: 'Armani',
+    pronunciation: 'armani',
+    color: '#f9f9f9'
+  }, {
+    key: 'z',
+    keycode: 90,
+    text: 'Givenchy',
+    pronunciation: 'givenchy',
+    color: '#fcfcfc'
+  }, {
+    key: 'x',
+    keycode: 88,
+    text: 'Hermès',
+    pronunciation: 'Hermès',
+    color: '#fafafa'
+  }, {
+    key: 'c',
+    keycode: 67,
+    text: 'Christian Dior',
+    pronunciation: 'christian dior',
+    color: '#f8f8f8'
+  }, {
+    key: 'r',
+    keycode: 82,
+    text: 'Balenciaga',
+    pronunciation: 'balenciaga',
+    color: '#fbfbfb'
+  }, {
+    key: 't',
+    keycode: 84,
+    text: 'Cartier',
+    pronunciation: 'Cartier',
+    color: '#f9f9f9'
+  }, {
+    key: 'y',
+    keycode: 89,
+    text: 'Bulgari',
+    pronunciation: 'bulgari',
+    color: '#f7f7f7'
+  }
+];
