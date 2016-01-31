53c9855e39f113b0dfd5ea8f5d3508c973a3031c
Neil Pullman <neil@descend.org>
2016-01-30 21:23:14 -0500

    Update 2016-01-31T02:23:12.372Z

--- scripts/app.js
+++ scripts/app.js
@@ -244,13 +244,11 @@ Object.defineProperty(exports, '__esModule', {
     value: true
 });
 var samples = [{ sample: 'clap' }, { sample: 'closedhihat' }, { sample: 'kick' }, { sample: 'openhihat' }, { sample: 'ride' }, { sample: 'shaker' }, { sample: 'snare' }, { sample: 'cymbal' }],
-    colors = ['#fcfcfc', '#fafafa', '#f8f8f8', '#f6f6f6', '#fbfbfb', '#f9f9f9', '#f7f7f7', '#f5f5f5'],
     keys = ['q', 'w', 'e', 'r', 'a', 's', 'd', 'f'];
 
 exports['default'] = samples.map(function (pad, i) {
     pad.active = false;
     pad.audio = new Howl({ urls: ['audio/' + pad.sample + '.WAV'] });
-    pad.color = colors[i];
     pad.key = keys[i];
     pad.keycode = keys[i].toUpperCase().charCodeAt(0);
 
