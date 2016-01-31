6c9e77957d9e518e75081c822788146385b8f4be
Neil Pullman <neil@descend.org>
2016-01-30 21:22:57 -0500

    dont define colors in pad.js

--- app/scripts/pads.js
+++ app/scripts/pads.js
@@ -8,14 +8,12 @@ let samples = [
         { sample: 'snare' },
         { sample: 'cymbal' }
     ],
-    colors = ['#fcfcfc', '#fafafa', '#f8f8f8', '#f6f6f6', '#fbfbfb', '#f9f9f9', '#f7f7f7', '#f5f5f5'],
     keys = ['q', 'w', 'e', 'r', 'a', 's', 'd', 'f'];
 
 
 export default samples.map((pad, i) => {
     pad.active = false;
     pad.audio = new Howl({ urls: ['audio/' + pad.sample + '.WAV'] })
-    pad.color = colors[i];
     pad.key = keys[i];
     pad.keycode = keys[i].toUpperCase().charCodeAt(0);
 
